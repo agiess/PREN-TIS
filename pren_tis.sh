605607c372a9dee29fa65f75a5368f7624d2a884
agiess <adam.giess@uib.no>
2017-11-21 23:04:14 +0100

    flag fix

--- pren_tis.sh
+++ pren_tis.sh
@@ -145,10 +145,10 @@ fi
 Rscript scripts/03_run_models.R ${out_dir}/${prefix}_positive.csv ${out_dir}/${prefix}_negative.csv ${out_dir}/${prefix}_stop2stop.csv ${out_dir}/model_metrics/${prefix}_training_glm_summary.csv ${out_dir}/model_metrics/${prefix}_training_glm_coefficients.csv ${out_dir}/model_metrics/${prefix}_training_randomforest_summary.csv ${out_dir}/model_metrics/${prefix}_training_randomforest_variable_importance.csv ${out_dir}/model_metrics/${prefix}_test_randomforest_performance_thresholds_and_metric_scores.csv ${out_dir}/model_metrics/${prefix}_test_randomforest_performance_confusion_matrix.csv ${out_dir}/model_metrics/${prefix}_test_randomforest_performance_summary.csv ${out_dir}/model_metrics/${prefix}_test_randomforest_performance_roc_plot.pdf ${out_dir}/${prefix}_stop2stop_predictions.csv
 
 #process GLM coefficients
-Rscript scripts/03_variable_importance_matrix_GLM.pl ${out_dir}/model_metrics/${prefix}_training_glm_coefficients.csv ${out_dir}/model_metrics/${prefix}_training_glm_coefficients_matrix.csv
+perl scripts/03_variable_importance_matrix_GLM.pl ${out_dir}/model_metrics/${prefix}_training_glm_coefficients.csv ${out_dir}/model_metrics/${prefix}_training_glm_coefficients_matrix.csv
 
 #process RF variable importances
-Rscript scripts/03_variable_importance_matrix_RF.pl ${out_dir}/model_metrics/${prefix}_training_randomforest_variable_importance.csv ${out_dir}/model_metrics/${prefix}_training_randomforest_variable_importance_matrix.csv 
+perl scripts/03_variable_importance_matrix_RF.pl ${out_dir}/model_metrics/${prefix}_training_randomforest_variable_importance.csv ${out_dir}/model_metrics/${prefix}_training_randomforest_variable_importance_matrix.csv 
 
 #plot variable importance heatmaps
 Rscript scripts/03_plot_variables.R ${out_dir}/model_metrics/${prefix}_training_glm_coefficients_matrix.csv ${out_dir}/model_metrics/${prefix}_training_randomforest_variable_importance_matrix.csv ${out_dir}/model_metrics/${prefix}_variable_importance_heatmaps.pdf
