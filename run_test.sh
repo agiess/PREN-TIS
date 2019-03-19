f5c4a813f281d238291ac7dd085bc79138110ad7
agiess <adam.giess@uib.no>
2019-03-19 12:18:54 +0100

    testing dataset

---
+++ run_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash pren_tis.sh -b data_for_testing/pSLT.bam -g data_for_testing/pSLT.gtf -f data_for_testing/pSLT.fa -v data_for_testing/pSLT_nterminal_validations.bed -o data_for_testing/test_out -t 1 -p 1.0
