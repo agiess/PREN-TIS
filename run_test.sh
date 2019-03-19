a28e308ebd0aef49e7b334fc7f690320505f0fef
agiess <adam.giess@uib.no>
2019-03-19 12:25:29 +0100

    testing dir

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+if [ ! -d data_for_testing/test_out ]; then
+    mkdir data_for_testing/test_out
+fi
+
 bash pren_tis.sh -b data_for_testing/pSLT.bam -g data_for_testing/pSLT.gtf -f data_for_testing/pSLT.fa -v data_for_testing/pSLT_nterminal_validations.bed -o data_for_testing/test_out -t 1 -p 1.0
